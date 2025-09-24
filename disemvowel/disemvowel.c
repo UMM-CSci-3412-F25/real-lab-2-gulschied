4c44c59930dcc037fff8bc393a4764e592d87ecf
JPGulo <gulon008@morris.umn.edu>
2025-09-23 19:33:20 -0500

    Created disemvowel function. Fixed memory leak in disemvowel.c freeing calloc usage.
    Co-Authored-By: Jakob Linscheid <linsc235@morris.umn.edu>

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,32 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-#include "disemvowel.h"
+static bool is_vowel(char c) {
+    char lower = (c >= 'A' && c <= 'Z') ? c + 32 : c; 
+    return (lower == 'a' || lower == 'e' || lower == 'i' ||
+            lower == 'o' || lower == 'u');
+}
 
 char *disemvowel(char *str) {
-  return (char*) "";
+    int len = strlen(str);
+    int count = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (!is_vowel(str[i])) {
+            count++;
+        }
+    }
+
+    char *result = (char *) calloc(count + 1, sizeof(char));
+
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+        if (!is_vowel(str[i])) {
+            result[j++] = str[i];
+        }
+    }
+    result[j] = '\0';
+
+    return result; 
 }
